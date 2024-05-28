7a0f8bcaa35765abd573b50cc57ec021381fc483
Nash Anderson <anderson.nash@gmail.com>
2024-05-28 15:37:14 -0500

    hero section with login and signup forms

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to snippets_path, notice: I18n.translate(".logged_in")
+      redirect_to root_url, notice: I18n.translate(".logged_in")
     else
       flash.now.alert = I18n.translate(".invalid")
       render "new"
@@ -15,6 +15,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to login_path, notice: I18n.translate('.logged_out')
+    redirect_to root_url, notice: I18n.translate('.logged_out')
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module SnippetApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.assets.paths << Rails.root.join('node_modules')
+    config.assets.precompile += %w( application.scss )
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
