a304c9ab682ea9cd7bc9b30f4da035bb4a66c5d5
Nash Anderson <anderson.nash@gmail.com>
2024-05-29 10:29:10 -0500

    adds create

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -1,11 +1,29 @@
 class SnippetsController < ApplicationController
   before_action :authorize, only: [:edit, :update]
-  
+
   def new
     @snippet = Snippet.new
   end
 
   def index
-    @snippet = Snippet.all
+    @snippets = Snippet.all
+  end
+
+  def show
+    @snippet = Snippet.find(params[:id])
+  end
+
+  def create
+    @snippet = current_user.snippets.build(snippet_params)
+    if @snippet.save
+      redirect_to @snippet, notice: 'Snippet was successfully created.'
+    else
+      render 'new'
+    end
+  end
+  private
+
+  def snippet_params
+    params.require(:snippet).permit(:title, :description, :code_block)
   end
 end
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,15 +1,25 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // or 'media' for media-query based dark mode
   content: [
-    './public/*.html',
+    './app/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*',
+    './app/assets/stylesheets/**/*.scss',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: {
+          background: '#f5f7fa',
+          text: '#1a202c',
+        },
+        dark: {
+          background: '#1a202c',
+          text: '#a0aec0',
+        },
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+  plugins: [],
 }
