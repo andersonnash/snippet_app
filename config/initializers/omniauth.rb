006f76d6f0e7052a60ecb2c2eedbf17dcc77de01
Nash Anderson <anderson.nash@gmail.com>
2024-06-22 16:35:40 -0700

    reconfigure oauth redirect for production and local

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,10 +7,9 @@
 #   }
 # end
 
-# config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
     scope: 'userinfo.email, userinfo.profile',
-    redirect_uri: "https://snippet-app-78766edea1d3.herokuapp.com/auth/google_oauth2/callback"
+    redirect_uri: Rails.env.production? ? ENV['PRODUCTION_REDIRECT_URI'] : ENV['DEVELOPMENT_REDIRECT_URI']
   }
 end
