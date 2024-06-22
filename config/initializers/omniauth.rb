82ce01520ca7b1de05a7977eda9fddb03878d01d
Nash Anderson <anderson.nash@gmail.com>
2024-06-21 19:25:31 -0600

    update OAuth redierct URI for production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,6 +7,10 @@
 #   }
 # end
 
+# config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
+    scope: 'userinfo.email, userinfo.profile',
+    redirect_uri: "https://snippet-app-78766edea1d3.herokuapp.com/auth/google_oauth2/callback"
+  }
 end
